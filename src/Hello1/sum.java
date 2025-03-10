55b4f257d893898ab8f046bdd28525caec22712c
chaithrakit2025 <chaithra.kit@gmail.com>
2025-03-10 14:18:43 -0400

    added sum file

---
+++ src/Hello1/sum.java
@@ -0,0 +1,14 @@
+package Hello1;
+
+public class sum {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int a=10;
+			int b=15;
+			int c=a+b;
+			System.out.println(" The sum of two numbers is = "+c);
+		}
+
+	}
+

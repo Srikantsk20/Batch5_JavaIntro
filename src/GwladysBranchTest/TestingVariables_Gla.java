0aa3edcdc435b6e82aef37fa2d7f3a3471274964
gnkazeu <cassy.casey2@gmail.com>
2025-03-13 22:30:44 -0400

    Add Testing Variable Gla Test

---
+++ src/GwladysBranchTest/TestingVariables_Gla.java
@@ -0,0 +1,11 @@
+package GwladysBranchTest;
+public class TestingVariables_Gla {
+    public static void main(String[] args) {
+         // Declare two numbers
+        int a = 2; 
+        int b = 3; 
+        int c = a + b;
+         // Print the result
+         System.out.println("The sum of " + a + " and " + b + " is: " + c);
+    }
+}

f7473beb8c9e1783dda5b956dc29a10e70fe741d
gnkazeu <cassy.casey2@gmail.com>
2025-03-14 00:13:51 -0400

    Add Boolean Test

---
+++ src/Boolean/Bool.java
@@ -0,0 +1,21 @@
+package Boolean;
+
+public class Bool{
+    public Bool() {
+    }
+ 
+    public static void main(String[] args) {
+    // Declare variable
+    int a = 15;
+
+    // Boolean in an if statement
+    if ((a % 5 == 0) && (a % 3 == 0)) {
+        System.out.println("FooBar");}
+    else if (a%5==0) {
+            System.out.println("Bar");}
+    else if (a % 3 == 0) {
+                System.out.println("Foo");
+            }}
+   
+} 
+

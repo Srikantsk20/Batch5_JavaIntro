2a183c2174dc907325ba24ddd50e8824049fa7c2
gnkazeu <cassy.casey2@gmail.com>
2025-03-13 23:05:01 -0400

    new change

--- src/GwladysBranchTest/TestingVariables_Gla.java
+++ src/GwladysBranchTest/TestingVariables_Gla.java
@@ -2,10 +2,11 @@ package GwladysBranchTest;
 public class TestingVariables_Gla {
     public static void main(String[] args) {
          // Declare two numbers
-        int a = 2; 
-        int b = 3; 
+        int a = 1; 
+        int b = 8; 
         int c = a + b;
          // Print the result
          System.out.println("The sum of " + a + " and " + b + " is: " + c);
+         System.out.println("I am awesome");
     }
 }

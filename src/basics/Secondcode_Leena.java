586be14214c029bfe354a4c5a2ff1a7da1205d85
Leena797 <khajoneneha797@gmail.com>
2025-03-15 13:07:43 -0400

    pushing new code

---
+++ src/basics/Secondcode_Leena.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class Secondcode_Leena {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World second time");
+	}
+
+}

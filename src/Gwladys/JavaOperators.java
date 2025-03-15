8d2b9679d0fe3e846690c93f6293dd948ad40523
gnkazeu <cassy.casey2@gmail.com>
2025-03-15 10:27:38 -0400

    Add Operators practice

---
+++ src/Gwladys/JavaOperators.java
@@ -0,0 +1,19 @@
+package Gwladys;
+
+public class JavaOperators {
+
+    public static void main(String[] args) {
+		// Learn operators
+
+        int a = 2;
+        int b = 3;
+        int c = 5;
+    boolean result = (a > b) && (c>b);
+        System.out.println("Result is" + result);
+
+        
+	}
+    
+}
+    
+
